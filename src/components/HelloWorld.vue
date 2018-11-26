e1695966b43d9694ae47cb74244ea578ca3a3beb
sue <2315172434@qq.com>
2018-11-26 21:56:53 +0800

    初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="container">
+<div class="card border-primary">
+  <img class="card-img-bottom" style="height:100px;" src="../assets/logo.png" alt="">
+  <div class="card-body">
+    <h4 class="card-title">Title</h4>
+    <p class="card-text">Text</p>
+  </div>
+</div>
+
+</div>
+
+</template>
+<script>
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      counter: 0
+    }
+  }
+}
+</script>
+<style>
+.container > div {
+  height: 500px;
+}
+</style>
