44bd12a570a07f0cd2c50827be0026f02d5a548f
sugar <2315172434@qq.com>
2018-11-30 17:38:21 +0800

    首页

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-fruild mp0">
     <div class="row">
-      <div id="head_carousel" class="col carousel slide" data-ride="carousel">
+      <div id="head_carousel" class="col carousel slide" data-ride="carousel" style="margin:0px;padding:0px;">
         <!-- 图片 -->
         <div class="carousel-inner">
           <div class="carousel-item active">
@@ -28,26 +28,33 @@
       </div>
     </div>
     <div class="head_nav">
-      <ul class="list-group">
-        <li class="list-group">
-          <ul class="nav nav-fill">
-            <li class="nav-item">
-              <a class="text-dark" href="#">全天房</a>
+      <ul class="">
+        <li class="" style="border-bottom: 1px solid #c0c0c0;">
+          <ul class="inter_type">
+            <li class="">
+              <span class="muted activate" href="#">全天房</span>
             </li>
-            <li class="nav-item">
-              <a class="muted" href="#">钟点房</a>
+            <li class="">
+              <span  class="muted" href="#">钟点房</span>
             </li>
-            <li class="nav-item">
-              <a class="muted" href="#">特价房</a>
+            <li class="">
+              <span class="muted" href="#">特价房</span>
             </li>
           </ul>
         </li>
-        <li class>
+        <li class="list_group_item">
           <span>武汉市</span>
-          <span class="ml-80%">我的位置</span>
+          <span class="ml-80%" style="float:right">我的位置</span>
         </li>
-        <li class>日期</li>
-        <li class>名称</li>
+        <li class="list_group_item">
+         <p style="width:80px;" class="dis_inline mp0">入住日期11/30</p>
+            <p style="width:80px;" class="dis_inline mp0">共两晚</p>
+         <p style="width:80px;" class="dis_inline mp0">离店日期11/31</p>
+        </li>
+        <li class="list_group_item">
+          <p style="width:80px;">离店日期12</p>
+
+        </li>
       </ul>
     </div>
   </div>
@@ -56,16 +63,30 @@
 #head_carousel img {
   height: 150px;
 }
-.head_nav > ul > li {
-  position: relative;
-  display: block;
-  padding: 0.75rem 3.25rem;
-  border-style: none none solid none;
-  border-color: #6c757d14;
+.list_group_item{
+  padding: 30px 40px;
+  border-bottom: 1px solid #c0c0c0;
 }
 
-.muted {
-  font-size: 14px;
-  color: #606060;
+.inter_type>li{
+  display: inline;
+  text-align: center;
 }
+.muted{
+    line-height: 24px;
+    padding: 12px 0px;
+    text-decoration: none;
+    display:inline-block;
+    width: 30%;
+    font-size: 14px;
+    color:#606266;
+}
+
+.activate{
+      color: #303133;
+      font-size: 16px;
+      font-family: fantasy;
+      border-bottom: 1px solid #409EFF;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import './assets/css/common.css'
+import './assets/css/style.css'
 import App from './App'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
-import './assets/css/style.css'
-import './assets/css/common.css'
 import './assets/icon/iconfont.css'
 Vue.config.productionTip = false
 
