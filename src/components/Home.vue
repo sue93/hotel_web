049b4f90064dc19e944d3561fd98bb26bfbe8f8b
sugar <2315172434@qq.com>
2018-12-03 17:44:35 +0800

    测试

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    sss
+<h1>{{name}}</h1>
+
+   <input  v-model.number="msg" type="number" placeholder="请输入年龄" value="">
+   <p>{{msg}}</p>
+</div>
+
+</template>
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: '',
+      name: '哈哈',
+      xc: '<h2>我们都是好孩子</h2>'
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/HelloWorld'
 import Layout from '@/components/LaYout'
+import home from '@/components/Home'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Layout',
       component: Layout
+    }, {
+      path: '/hello',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }, {
+      path: '/home',
+      name: 'HOme',
+      component: home
     }
   ]
 })
